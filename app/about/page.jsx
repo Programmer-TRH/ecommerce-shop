2c5fd376939378afb0eb7056bc6c383a0106baef
Programmer-TRH <tokdirrahoman4@gmail.com>
2024-12-28 21:25:10 +0600

    AboutPage Start

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import HighLightedSection from '@/components/AboutPage/HighLightedSection'
+import StorySection from '@/components/AboutPage/StorySection'
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <StorySection />
+      <HighLightedSection />
+    </div>
+  )
+}
+ 
--- components/Common/Title.jsx
+++ components/Common/Title.jsx
@@ -1,8 +1,9 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
-export default function Title({text}) {
+export default function Title({text, className=""}) {
   return (
-    <h2 className={`text-2xl md:text-3xl lg:text-4xl font-semibold text-primary-black`}>
+    <h2 className={cn("text-2xl md:text-3xl lg:text-4xl font-semibold text-primary-black", className)}>
         {text}
     </h2>
   )
